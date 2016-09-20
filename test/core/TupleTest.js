656dfaddc7c69760a0841746271fd0390d6eb1c2
Graeme Lockley <graemel@no9.co.za>
2016-09-20 08:10:52 +0200

    Upgraded to use ES6 features and simplified the structure of the test.

--- test/core/TupleTest.js
+++ test/core/TupleTest.js
@@ -6,36 +6,24 @@ var Chai = require("../../bower_components/chai/chai");
 var expect = Chai.expect;
 
 describe('Core/Tuple', function () {
-    describe('given a tuple of (10, "abc")', function () {
+    describe('given a tuple of (10, "abc")', () => {
         var tuple = Tuple.Tuple(10, 'abc');
 
-        it('fst should return 10', function () {
-            expect(Tuple.fst(tuple)).to.equal(10);
-        });
-        it('snd should return "abc"', function () {
-            expect(Tuple.snd(tuple)).to.equal('abc');
-        });
+        it('fst should return 10', () => expect(Tuple.fst(tuple)).to.equal(10));
+        it('snd should return "abc"', () => expect(Tuple.snd(tuple)).to.equal('abc'));
 
-        describe('setFst(20) on tuple', function() {
+        describe('setFst(20) on tuple', () => {
             var tuple2 = Tuple.setFst(20, tuple);
 
-            it('fst should return 20', function () {
-                expect(Tuple.fst(tuple2)).to.equal(20);
-            });
-            it('snd should return "abc"', function () {
-                expect(Tuple.snd(tuple2)).to.equal('abc');
-            });
+            it('fst should return 20', () => expect(Tuple.fst(tuple2)).to.equal(20));
+            it('snd should return "abc"', () => expect(Tuple.snd(tuple2)).to.equal('abc'));
         });
 
-        describe('setSnd(100) on tuple', function() {
+        describe('setSnd(100) on tuple', () => {
             var tuple2 = Tuple.setSnd(100, tuple);
 
-            it('fst should return 10', function () {
-                expect(Tuple.fst(tuple2)).to.equal(10);
-            });
-            it('snd should return 100', function () {
-                expect(Tuple.snd(tuple2)).to.equal(100);
-            });
+            it('fst should return 10', () => expect(Tuple.fst(tuple2)).to.equal(10));
+            it('snd should return 100', () => expect(Tuple.snd(tuple2)).to.equal(100));
         });
     });
 });
