88c6277923f2e5734a0c93604f3b39c11e3a5e71
Graeme Lockley <graemel@no9.co.za>
2016-09-20 19:49:37 +0200

    Simplified the tests

--- test/core/ResultTest.js
+++ test/core/ResultTest.js
@@ -6,37 +6,21 @@ var Chai = require("../../bower_components/chai/chai");
 var expect = Chai.expect;
 
 describe('Core/Result', function () {
-    describe('given an Ok created with a content of 123', function () {
+    describe('given an Ok created with a content of 123', () => {
         var result = Result.Ok(123);
 
-        it('should return true for isOk', function () {
-            expect(result.isOk()).to.equal(true);
-        });
-        it('should return false for isError', function () {
-            expect(result.isError()).to.equal(false);
-        });
-        it('should return 123 when asking getOkOrElse', function () {
-            expect(result.getOkOrElse(-1)).to.equal(123);
-        });
-        it('should return -1 when asking getErrorOrElse', function () {
-            expect(result.getErrorOrElse(-1)).to.equal(-1);
-        });
+        it('should return true for isOk', () => expect(result.isOk()).to.equal(true));
+        it('should return false for isError', () => expect(result.isError()).to.equal(false));
+        it('should return 123 when asking getOkOrElse', () => expect(result.getOkOrElse(-1)).to.equal(123));
+        it('should return -1 when asking getErrorOrElse', () => expect(result.getErrorOrElse(-1)).to.equal(-1));
     });
 
-    describe('given an Error created with a content of 123', function () {
+    describe('given an Error created with a content of 123', () => {
         var result = Result.Error(123);
 
-        it('should return false for isOk', function () {
-            expect(result.isOk()).to.equal(false);
-        });
-        it('should return true for isError', function () {
-            expect(result.isError()).to.equal(true);
-        });
-        it('should return -1 when asking getOkOrElse', function () {
-            expect(result.getOkOrElse(-1)).to.equal(-1);
-        });
-        it('should return 123 when asking getErrorOrElse', function () {
-            expect(result.getErrorOrElse(-1)).to.equal(123);
-        });
+        it('should return false for isOk', () => expect(result.isOk()).to.equal(false));
+        it('should return true for isError', () => expect(result.isError()).to.equal(true));
+        it('should return -1 when asking getOkOrElse', () => expect(result.getOkOrElse(-1)).to.equal(-1));
+        it('should return 123 when asking getErrorOrElse', () => expect(result.getErrorOrElse(-1)).to.equal(123));
     });
 });
