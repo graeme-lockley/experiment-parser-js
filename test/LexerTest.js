5b56da63a8b891d3e73346eed6d43978f5057d28
Graeme Lockley <graemel@no9.co.za>
2016-09-20 19:59:45 +0200

    Upgraded to use ES6 features

--- test/LexerTest.js
+++ test/LexerTest.js
@@ -6,91 +6,68 @@ var Chai = require("../bower_components/chai/chai");
 var expect = Chai.expect;
 
 describe('Lexer', function () {
-    describe('with input "Hello 123"', function () {
+    describe('with input "Hello 123"', () => {
         var context = Lexer.fromString('Hello 123');
 
-        describe('after initialisation', function () {
-            it('should match to an IDENTIFIER', function () {
-                expect(context.token.id).to.equal(Lexer.TokenEnum.IDENTIFIER);
-            });
-            it('should have the coordinates (1, 1)', function () {
+        describe('after initialisation', () => {
+            it('should match to an IDENTIFIER', () => expect(context.token.id).to.equal(Lexer.TokenEnum.IDENTIFIER));
+            it('should have the coordinates (1, 1)', () => {
                 expect(context.token.x).to.equal(1);
                 expect(context.token.y).to.equal(1);
             });
-            it('should have the text "Hello"', function () {
-                expect(context.token.text).to.equal('Hello');
-            });
+            it('should have the text "Hello"', () => expect(context.token.text).to.equal('Hello'));
         });
 
-        describe('next token', function () {
+        describe('next token', () => {
             var nextContext = context.next();
 
-            it('should match to a CONSTANT_INTEGER', function () {
-                expect(nextContext.token.id).to.equal(Lexer.TokenEnum.CONSTANT_INTEGER);
-            });
-            it('should have the coordinates (7, 1)', function () {
+            it('should match to a CONSTANT_INTEGER', () => expect(nextContext.token.id).to.equal(Lexer.TokenEnum.CONSTANT_INTEGER));
+            it('should have the coordinates (7, 1)', ()=> {
                 expect(nextContext.token.x).to.equal(7);
                 expect(nextContext.token.y).to.equal(1);
             });
-            it('should have the text "123"', function () {
-                expect(nextContext.token.text).to.equal("123");
-            });
+            it('should have the text "123"', () => expect(nextContext.token.text).to.equal("123"));
         });
 
-        describe('next next token', function () {
+        describe('next next token', () => {
             var nextNextContext = context.next().next();
 
-            it('should report end-of-file', function () {
-                expect(nextNextContext.token.id).to.equal(Lexer.TokenEnum.EOF);
-            });
-            it('should have the coordinates (10, 1)', function () {
+            it('should report end-of-file', () => expect(nextNextContext.token.id).to.equal(Lexer.TokenEnum.EOF));
+            it('should have the coordinates (10, 1)', ()=> {
                 expect(nextNextContext.token.x).to.equal(10);
                 expect(nextNextContext.token.y).to.equal(1);
             });
-            it('should have the text ""', function () {
-                expect(nextNextContext.token.text).to.equal("");
-            });
+            it('should have the text ""', () => expect(nextNextContext.token.text).to.equal(""));
         });
 
-        describe('next next next token', function () {
+        describe('next next next token', () => {
             var nextNextContext = context.next().next().next();
 
-            it('should still report end-of-file', function () {
-                expect(nextNextContext.token.id).to.equal(Lexer.TokenEnum.EOF);
-            });
-            it('should have the coordinates (10, 1)', function () {
+            it('should still report end-of-file', () => expect(nextNextContext.token.id).to.equal(Lexer.TokenEnum.EOF));
+            it('should have the coordinates (10, 1)', () => {
                 expect(nextNextContext.token.x).to.equal(10);
                 expect(nextNextContext.token.y).to.equal(1);
             });
-            it('should have the text ""', function () {
-                expect(nextNextContext.token.text).to.equal("");
-            });
+            it('should have the text ""', () => expect(nextNextContext.token.text).to.equal(""));
         });
     });
 
-    describe('with input "a\\ b) c("', function() {
+    describe('with input "a\\ b) c("', function () {
         var context = Lexer.fromString('a\\ b) c(');
 
-        it('first token should be IDENTIFIER', function() {
-            expect(context.token.id).to.equal(Lexer.TokenEnum.IDENTIFIER);
-        });
-        it('second token should be LAMBDA', function() {
-            expect(context.next().token.id).to.equal(Lexer.TokenEnum.LAMBDA);
-        });
-        it('third token should be IDENTIFIER', function() {
-            expect(context.next().next().token.id).to.equal(Lexer.TokenEnum.IDENTIFIER);
-        });
-        it('forth token should be RPAREN', function() {
-            expect(context.next().next().next().token.id).to.equal(Lexer.TokenEnum.RPAREN);
-        });
-        it('fifth token should be IDENTIFIER', function() {
-            expect(context.next().next().next().next().token.id).to.equal(Lexer.TokenEnum.IDENTIFIER);
-        });
-        it('sixth token should be LPAREN', function() {
-            expect(context.next().next().next().next().next().token.id).to.equal(Lexer.TokenEnum.LPAREN);
-        });
-        it('seventh token should be EOF', function() {
-            expect(context.next().next().next().next().next().next().token.id).to.equal(Lexer.TokenEnum.EOF);
-        });
+        it('first token should be IDENTIFIER', () =>
+            expect(context.token.id).to.equal(Lexer.TokenEnum.IDENTIFIER));
+        it('second token should be LAMBDA', () =>
+            expect(context.next().token.id).to.equal(Lexer.TokenEnum.LAMBDA));
+        it('third token should be IDENTIFIER', () =>
+            expect(context.next().next().token.id).to.equal(Lexer.TokenEnum.IDENTIFIER));
+        it('forth token should be RPAREN', () =>
+            expect(context.next().next().next().token.id).to.equal(Lexer.TokenEnum.RPAREN));
+        it('fifth token should be IDENTIFIER', () =>
+            expect(context.next().next().next().next().token.id).to.equal(Lexer.TokenEnum.IDENTIFIER));
+        it('sixth token should be LPAREN', () =>
+            expect(context.next().next().next().next().next().token.id).to.equal(Lexer.TokenEnum.LPAREN));
+        it('seventh token should be EOF', () =>
+            expect(context.next().next().next().next().next().next().token.id).to.equal(Lexer.TokenEnum.EOF));
     });
 });
--- test/ParserTest.js
+++ test/ParserTest.js
@@ -10,65 +10,56 @@ var Chai = require("../bower_components/chai/chai");
 var expect = Chai.expect;
 
 describe('Parser', function () {
-    describe('given the input "123" to parseTerm', function () {
+    describe('given the input "123" to parseTerm', () => {
         var result = Parser.parseTerm(Lexer.fromString("123"));
 
-        it("should parse without any errors", function () {
-            expect(result.isOk()).to.equal(true);
-        });
-        it("should parse a CONSTANT_INTEGER with value 123", function () {
+        it("should parse without any errors", () =>
+            expect(result.isOk()).to.equal(true));
+        it("should parse a CONSTANT_INTEGER with value 123", () => {
             expect(result.getOkOrElse().fst.type).to.equal(AST.ASTEnum.CONSTANT_INTEGER);
             expect(result.getOkOrElse().fst.value).to.equal(123);
         });
-        it("should have the next token of EOF", function () {
-            expect(result.getOkOrElse().snd.token.id).to.equal(Lexer.TokenEnum.EOF);
-        });
+        it("should have the next token of EOF", () =>
+            expect(result.getOkOrElse().snd.token.id).to.equal(Lexer.TokenEnum.EOF));
     });
 
-    describe('given the input "abc 123" to parseTerm', function () {
+    describe('given the input "abc 123" to parseTerm', () => {
         var result = Parser.parseTerm(Lexer.fromString("abc 123"));
 
-        it("should parse without any errors", function () {
-            expect(result.isOk()).to.equal(true);
-        });
-        it('should parse an IDENTIFIER with value "abc"', function () {
+        it("should parse without any errors", () =>
+            expect(result.isOk()).to.equal(true));
+        it('should parse an IDENTIFIER with value "abc"', () => {
             expect(result.getOkOrElse().fst.type).to.equal(AST.ASTEnum.IDENTIFIER);
             expect(result.getOkOrElse().fst.name).to.equal('abc');
         });
-        it("should have the next token of CONSTANT_INTEGER", function () {
-            expect(result.getOkOrElse().snd.token.id).to.equal(Lexer.TokenEnum.CONSTANT_INTEGER);
-        });
+        it("should have the next token of CONSTANT_INTEGER", () =>
+            expect(result.getOkOrElse().snd.token.id).to.equal(Lexer.TokenEnum.CONSTANT_INTEGER));
     });
 
-    describe('given the input "( 123)" to parseTerm', function () {
+    describe('given the input "( 123)" to parseTerm', () => {
         var result = Parser.parseTerm(Lexer.fromString('( 123)'));
 
-        it("should parse without any errors", function () {
-            expect(result.isOk(result)).to.equal(true);
-        });
-        it("should parse a CONSTANT_INTEGER with value 123", function () {
+        it("should parse without any errors", ()=>
+            expect(result.isOk(result)).to.equal(true));
+        it("should parse a CONSTANT_INTEGER with value 123", () => {
             expect(result.getOkOrElse().fst.type).to.equal(AST.ASTEnum.CONSTANT_INTEGER);
             expect(result.getOkOrElse().fst.value).to.equal(123);
         });
-        it("should have the next token of EOF", function () {
-            expect(result.getOkOrElse().snd.token.id).to.equal(Lexer.TokenEnum.EOF);
-        });
+        it("should have the next token of EOF", () =>
+            expect(result.getOkOrElse().snd.token.id).to.equal(Lexer.TokenEnum.EOF));
     });
 
-    describe('given the input "\\a \\b -> (a)" to parseTerm', function () {
+    describe('given the input "\\a \\b -> (a)" to parseTerm', () => {
         var result = Parser.parseTerm(Lexer.fromString('\\a \\b -> (a)'));
 
-        it("should parse without any errors", function () {
-            expect(result.isOk()).to.equal(true);
-        });
-        it("should parse a LAMBDA with variables ['a', 'b'] and expression of IDENTIFIER with value 'a'", function () {
+        it("should parse without any errors", () => expect(result.isOk()).to.equal(true));
+        it("should parse a LAMBDA with variables ['a', 'b'] and expression of IDENTIFIER with value 'a'", () => {
             expect(result.getOkOrElse().fst.type).to.equal(AST.ASTEnum.LAMBDA);
             expect(result.getOkOrElse().fst.variables.length).to.equal(2);
             expect(result.getOkOrElse().fst.variables[0]).to.equal('a');
             expect(result.getOkOrElse().fst.variables[1]).to.equal('b');
         });
-        it("should have the next token of EOF", function () {
-            expect(result.getOkOrElse().snd.token.id).to.equal(Lexer.TokenEnum.EOF);
-        });
+        it("should have the next token of EOF", () =>
+            expect(result.getOkOrElse().snd.token.id).to.equal(Lexer.TokenEnum.EOF));
     });
 });
