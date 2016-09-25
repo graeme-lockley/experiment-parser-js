36b1c79c751db186ec79f0cdcdfc4f1498afc031
Graeme Lockley <graemel@no9.co.za>
2016-09-25 20:25:30 +0200

    feat: add findFirst against an array to return an Option

--- src/core/Array.js
+++ src/core/Array.js
@@ -1,3 +1,8 @@
+"use strict";
+
+const Optional = require('./Option');
+
+
 function foldr(initValue, foldFunction, arr) {
     let result = initValue;
 
@@ -8,6 +13,17 @@ function foldr(initValue, foldFunction, arr) {
     return result;
 }
 
+
+function findFirst(predicate, arr) {
+    for (let index = 0; index < arr.length; index += 1) {
+        if (predicate(arr[index])) {
+            return Optional.Some(arr[index]);
+        }
+    }
+    return Optional.None;
+}
+
+
 module.exports = {
-    foldr
+    foldr, findFirst
 };
