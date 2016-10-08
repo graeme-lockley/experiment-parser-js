b15cac676b3a11ddc7cdbcfecf3ccfc7f6d88d74
Graeme Lockley <graemel@no9.co.za>
2016-10-08 12:04:03 +0200

    test: add a test to calculate the length of a list

--- src/Translator.js
+++ src/Translator.js
@@ -25,6 +25,10 @@ function astToJavascript(ast, indentation = 0) {
         return ast.name;
     } else if (ast instanceof AST.If) {
         return astToJavascript(ast.ifExpr, indentation) + "\n" + spaces(indentation + 1) + "? " + astToJavascript(ast.thenExpr, indentation + 1) + "\n" + spaces(indentation + 1) + ": " + astToJavascript(ast.elseExpr, indentation + 1);
+    } else if (ast instanceof AST.Import) {
+        const fileName = ast.url.value.substring(5);
+
+        return 'const ' + ast.id.name + " = require('" + fileName + "');";
     } else if (ast instanceof AST.Lambda) {
         const tmpResult = Array.foldr(astToJavascript(ast.expression, indentation), (accumulator, item) => "(" + item + " => " + accumulator + ")", ast.variables);
         return tmpResult.substr(1, tmpResult.length - 2);
@@ -33,9 +37,14 @@ function astToJavascript(ast, indentation = 0) {
     } else if (ast instanceof AST.LessThanEqual) {
         return astToJavascript(ast.left, indentation) + " <= " + astToJavascript(ast.right, indentation);
     } else if (ast instanceof AST.Module) {
-        return ast.declarations.map(d => astToJavascript(d, indentation)).join('\n\n');
+        const imports = ast.imports.map(i => astToJavascript(i, indentation)).join('\n');
+
+        return (imports.length == 0 ? '' : imports + '\n\n') +
+               ast.declarations.map(d => astToJavascript(d, indentation)).join('\n\n');
     } else if (ast instanceof AST.Subtraction) {
         return astToJavascript(ast.left, indentation) + " - " + astToJavascript(ast.right, indentation);
+    } else if (ast instanceof AST.QualifiedIdentifier) {
+        return ast.module + "." + ast.identifier;
     }
 }
 
