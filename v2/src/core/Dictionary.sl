e882b2c814d65fdc9675344e18517926d069e35c
Graeme Lockley <graemel@no9.co.za>
2016-10-22 07:22:34 +0200

    feat: add a simple Dictionary module to create constant dictionaries

---
+++ v2/src/core/Dictionary.sl
@@ -0,0 +1,25 @@
+import file:./DictionaryHelper as Helper;
+
+
+mk0 =
+    Helper.mk0;
+
+
+mk1 =
+    Helper.mk1;
+
+
+mk2 =
+    Helper.mk2;
+
+
+mk3 =
+    Helper.mk3;
+
+
+mk4 =
+    Helper.mk4;
+
+
+mk5 =
+    Helper.mk5;
---
+++ v2/src/core/DictionaryHelper.js
@@ -0,0 +1,71 @@
+"use strict";
+
+
+function mk0() {
+    return {};
+}
+
+
+function mk1(k1) {
+    return v1 => {
+        const result = {};
+        result[k1] = v1;
+        return result;
+    };
+}
+
+
+function mk2(k1) {
+    return v1 => k2 => v2 => {
+        const result = {};
+        result[k1] = v1;
+        result[k2] = v2;
+        return result;
+    };
+}
+
+
+function mk3(k1) {
+    return v1 => k2 => v2 => k3 => v3 => {
+        const result = {};
+        result[k1] = v1;
+        result[k2] = v2;
+        result[k3] = v3;
+        return result;
+    };
+}
+
+
+function mk4(k1) {
+    return v1 => k2 => v2 => k3 => v3 => k4 => v4 => {
+        const result = {};
+        result[k1] = v1;
+        result[k2] = v2;
+        result[k3] = v3;
+        result[k4] = v4;
+        return result;
+    };
+}
+
+
+function mk5(k1) {
+    return v1 => k2 => v2 => k3 => v3 => k4 => v4 => k5 => v5 => {
+        const result = {};
+        result[k1] = v1;
+        result[k2] = v2;
+        result[k3] = v3;
+        result[k4] = v4;
+        result[k5] = v5;
+        return result;
+    };
+}
+
+
+module.exports = {
+    mk0,
+    mk1,
+    mk2,
+    mk3,
+    mk4,
+    mk5
+};
