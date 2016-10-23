f2acb8162d5dc197c0a3144acd4239da42e32677
Graeme Lockley <graemel@no9.co.za>
2016-10-23 09:41:18 +0200

    refactor: move Cursor out of Context

--- v2/src/compiler/LexerHelper.js
+++ v2/src/compiler/LexerHelper.js
@@ -115,6 +115,74 @@ function isIdentifierRest(c) {
         c == '_'.charCodeAt(0) || c == '\''.charCodeAt(0);
 }
 
+function createACursor(context) {
+    return {
+        index: context.index,
+        indexX: context.indexX,
+        indexY: context.indexY,
+
+        _indexXY: context.index,
+        x: context.indexX,
+        y: context.indexY,
+
+        content: context.input.content,
+        length: context.input.length,
+
+        charCodeAtIndex: function () {
+            return this.content.charCodeAt(this.index);
+        },
+
+        is: function (predicate) {
+            return this.isNotEndOfFile() && predicate(this.charCodeAtIndex());
+        },
+        isNot: function (predicate) {
+            return this.isNotEndOfFile() && !predicate(this.charCodeAtIndex());
+        },
+        isChar: function (c) {
+            return this.isNotEndOfFile() && this.charCodeAtIndex() == c.charCodeAt(0);
+        },
+        isNotChar: function (c) {
+            return this.isNotEndOfFile() && this.charCodeAtIndex() != c.charCodeAt(0);
+        },
+        isEndOfFile: function () {
+            return this.index >= this.length;
+        },
+        isNotEndOfFile: function () {
+            return !this.isEndOfFile();
+        },
+        advanceIndex: function () {
+            if (this.isNotEndOfFile()) {
+                if (this.is(isEndOfLine)) {
+                    this.indexX = 1;
+                    this.indexY += 1;
+                } else {
+                    this.indexX += 1;
+                }
+                this.index += 1;
+            }
+        },
+        markStartOfToken: function () {
+            this._indexXY = this.index;
+            this.x = this.indexX;
+            this.y = this.indexY;
+        },
+        text: function () {
+            return this.content.substr(this._indexXY, this.index - this._indexXY);
+        },
+        clone: function () {
+            var temp = this.constructor();
+            for (var key in this) {
+                if (this.hasOwnProperty(key)) {
+                    temp[key] = this[key];
+                }
+            }
+
+            return temp;
+        }
+    };
+}
+
+
 class Context {
     constructor(input, id, x, y, index, indexX, indexY, indexXY, text) {
         this.input = input;
@@ -129,70 +197,7 @@ class Context {
     }
 
     createCursor() {
-        return {
-            index: this.index,
-            indexX: this.indexX,
-            indexY: this.indexY,
-
-            _indexXY: this.index,
-            x: this.indexX,
-            y: this.indexY,
-
-            content: this.input.content,
-            length: this.input.length,
-
-            charCodeAtIndex: function () {
-                return this.content.charCodeAt(this.index);
-            },
-
-            is: function (predicate) {
-                return this.isNotEndOfFile() && predicate(this.charCodeAtIndex());
-            },
-            isNot: function (predicate) {
-                return this.isNotEndOfFile() && !predicate(this.charCodeAtIndex());
-            },
-            isChar: function (c) {
-                return this.isNotEndOfFile() && this.charCodeAtIndex() == c.charCodeAt(0);
-            },
-            isNotChar: function (c) {
-                return this.isNotEndOfFile() && this.charCodeAtIndex() != c.charCodeAt(0);
-            },
-            isEndOfFile: function () {
-                return this.index >= this.length;
-            },
-            isNotEndOfFile: function () {
-                return !this.isEndOfFile();
-            },
-            advanceIndex: function () {
-                if (this.isNotEndOfFile()) {
-                    if (this.is(isEndOfLine)) {
-                        this.indexX = 1;
-                        this.indexY += 1;
-                    } else {
-                        this.indexX += 1;
-                    }
-                    this.index += 1;
-                }
-            },
-            markStartOfToken: function () {
-                this._indexXY = this.index;
-                this.x = this.indexX;
-                this.y = this.indexY;
-            },
-            text: function () {
-                return this.content.substr(this._indexXY, this.index - this._indexXY);
-            },
-            clone: function () {
-                var temp = this.constructor();
-                for (var key in this) {
-                    if (this.hasOwnProperty(key)) {
-                        temp[key] = this[key];
-                    }
-                }
-
-                return temp;
-            }
-        };
+        return createACursor(this);
     }
 
     next() {
