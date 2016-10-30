efeafb13f95c7c2ca170eaa4dcfe102503c1fb0f
Graeme Lockley <graemel@no9.co.za>
2016-10-30 09:32:01 +0200

    refactor: remove dead code

--- v2/src/compiler/TranslatorHelper.js
+++ v2/src/compiler/TranslatorHelper.js
@@ -29,79 +29,6 @@ function encodeString(s) {
 }
 
 
-function astToJavascript(ast) {
-    return indentation => {
-
-        function spaces(count) {
-            return '  '.repeat(count);
-        }
-
-        if (ast.type == "ADDITION") {
-            return '(' + astToJavascript(ast.left)(indentation) + " + " + astToJavascript(ast.right)(indentation) + ')';
-        } else if (ast.type == "APPLY") {
-            return astToJavascript(ast.expressions[0])(indentation) + ast.expressions.slice(1).map(x => "(" + astToJavascript(x)(indentation) + ")").join('');
-        } else if (ast.type == "BOOLEAN_AND") {
-            return '(' + ast.expressions.map(e => astToJavascript(e)(indentation)).join(' && ') + ')';
-        } else if (ast.type == "BOOLEAN_NOT") {
-            return '(!' + astToJavascript(ast.operand)(indentation) + ')';
-        } else if (ast.type == "BOOLEAN_OR") {
-            return '(' + ast.expressions.map(e => astToJavascript(e)(indentation)).join(' || ') + ')';
-        } else if (ast.type == "COMPOSITION") {
-            const variableName = '_$' + indentation;
-            return '(' + variableName + ' => ' + astToJavascript(ast.left)(indentation) + '(' + astToJavascript(ast.right)(indentation) + '(' + variableName + ')))';
-        } else if (ast.type == "CONSTANT_BOOLEAN") {
-            return ast.value ? 'true' : 'false';
-        } else if (ast.type == "CONSTANT_CHARACTER") {
-            return '"' + encodeString(ast.value) + '"';
-        } else if (ast.type == "CONSTANT_INTEGER") {
-            return ast.value;
-        } else if (ast.type == "CONSTANT_STRING") {
-            return '"' + encodeString(ast.value) + '"';
-        } else if (ast.type == "CONSTANT_UNIT") {
-            return 'undefined';
-        } else if (ast.type == "DIVISION") {
-            return '(' + astToJavascript(ast.left)(indentation) + ' / ' + astToJavascript(ast.right)(indentation) + ')';
-        } else if (ast.type == "EQUAL") {
-            return '(' + astToJavascript(ast.left)(indentation) + " == " + astToJavascript(ast.right)(indentation) + ')';
-        } else if (ast.type == "EXPRESSIONS") {
-            return '(() => {\n' + spaces(indentation + 2)
-                + ast.expressions.slice(0, ast.expressions.length - 1).map(e => astToJavascript(e)(indentation + 2)).join(";\n" + spaces(indentation + 2))
-                + (ast.expressions.length > 1 ? ';\n' : '')
-                + spaces(indentation + 2) + 'return ' + astToJavascript(ast.expressions[ast.expressions.length - 1])(0) + ';\n'
-                + spaces(indentation + 1) + "})()";
-        } else if (ast.type == "GREATER_THAN") {
-            return '(' + astToJavascript(ast.left)(indentation) + " > " + astToJavascript(ast.right)(indentation) + ')';
-        } else if (ast.type == "GREATER_THAN_EQUAL") {
-            return '(' + astToJavascript(ast.left)(indentation) + " >= " + astToJavascript(ast.right)(indentation) + ')';
-        } else if (ast.type == "IDENTIFIER") {
-            return ast.name;
-        } else if (ast.type == "IF") {
-            return '(' + astToJavascript(ast.ifExpr)(indentation) + "\n" + spaces(indentation + 1) + "? " + astToJavascript(ast.thenExpr)(indentation + 1) + "\n" + spaces(indentation + 1) + ": " + astToJavascript(ast.elseExpr)(indentation + 1) + ')';
-        } else if (ast.type == "INFIX_OPERATOR") {
-            return infixOperators[ast.operator];
-        } else if (ast.type == "LAMBDA") {
-            const tmpResult = '(' + Array.foldr(accumulator => item => "(" + item + " => " + accumulator + ")")(astToJavascript(ast.expression)(indentation))(ast.variables) + ')';
-            return tmpResult.substr(1, tmpResult.length - 2);
-        } else if (ast.type == "LESS_THAN") {
-            return '(' + astToJavascript(ast.left)(indentation) + " < " + astToJavascript(ast.right)(indentation) + ')';
-        } else if (ast.type == "LESS_THAN_EQUAL") {
-            return '(' + astToJavascript(ast.left)(indentation) + " <= " + astToJavascript(ast.right)(indentation) + ')';
-        } else if (ast.type == "MULTIPLICATION") {
-            return '(' + astToJavascript(ast.left)(indentation) + " * " + astToJavascript(ast.right)(indentation) + ')';
-        } else if (ast.type == "QUALIFIED_IDENTIFIER") {
-            return ast.module + "." + ast.identifier;
-        } else if (ast.type == "STRING_CONCAT") {
-            return '(' + astToJavascript(ast.left)(indentation) + " + " + astToJavascript(ast.right)(indentation) + ')';
-        } else if (ast.type == "SUBTRACTION") {
-            return '(' + astToJavascript(ast.left)(indentation) + " - " + astToJavascript(ast.right)(indentation) + ')';
-        } else if (ast.type == "UNARY_PLUS") {
-            return '(+' + astToJavascript(ast.operand)(indentation) + ')';
-        } else if (ast.type == "UNARY_NEGATE") {
-            return '(-' + astToJavascript(ast.operand)(indentation) + ')';
-        }
-    };
-}
-
 
 function simplifyPath(path) {
     let candidateResult = path;
@@ -117,7 +44,6 @@ function simplifyPath(path) {
 
 
 module.exports = {
-    astToJavascript,
     infixOperators,
     encodeString,
     simplifyPath
